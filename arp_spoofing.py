03f1872fe90ee36eaf9a1fdb28160c6b699b1a78
PtspluS <pierre.marez@isen.yncrea.fr>
2020-09-06 19:13:54 +0200

    v 0.0.1

---
+++ arp_spoofing.py
@@ -0,0 +1,75 @@
+import getopt, sys
+import daemon_arp_spoofing
+
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "t:vahoV", ["time="])
+    except getopt.GetoptError as err:
+        print(err)
+        print(print_help())
+        sys.exit(2)
+
+    # options params
+    time = 3000
+    verbose = False
+    active = False
+    output = False
+    output_file = ""
+
+    for o, a in opts:
+        if o in ('-t', "--time="):
+            time = a
+        elif o == '-v':
+            verbose = True
+        elif o == '-a':
+            active = True
+        elif o == '-o':
+            output = True
+        elif o == '-V':
+            print(print_version())
+            sys.exit()
+        elif o == '-h':
+            print(print_logo())
+            print(print_help())
+            sys.exit()
+
+    d = daemon_arp_spoofing(time=time, verbose=verbose, active=active, output=output, output_file=output_file)
+    d.run()
+
+def print_logo():
+    return """
+ $$$$$$\                                                                       $$$$$$\  $$\                     
+$$  __$$\                                                                     $$  __$$\ \__|                    
+$$ /  $$ | $$$$$$\   $$$$$$\           $$$$$$$\  $$$$$$\   $$$$$$\   $$$$$$\  $$ /  \__|$$\ $$$$$$$\   $$$$$$\  
+$$$$$$$$ |$$  __$$\ $$  __$$\         $$  _____|$$  __$$\ $$  __$$\ $$  __$$\ $$$$\     $$ |$$  __$$\ $$  __$$\ 
+$$  __$$ |$$ |  \__|$$ /  $$ |        \$$$$$$\  $$ /  $$ |$$ /  $$ |$$ /  $$ |$$  _|    $$ |$$ |  $$ |$$ /  $$ |
+$$ |  $$ |$$ |      $$ |  $$ |         \____$$\ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |$$ |  $$ |$$ |  $$ |
+$$ |  $$ |$$ |      $$$$$$$  |        $$$$$$$  |$$$$$$$  |\$$$$$$  |\$$$$$$  |$$ |      $$ |$$ |  $$ |\$$$$$$$ |
+\__|  \__|\__|      $$  ____/ $$$$$$\ \_______/ $$  ____/  \______/  \______/ \__|      \__|\__|  \__| \____$$ |
+                    $$ |      \______|          $$ |                                                  $$\   $$ |
+                    $$ |                        $$ |                                                  \$$$$$$  |
+                    \__|                        \__|                                                   \______/ 
+_________________________________________________________________________________________________________________
+    """
+
+def print_help():
+    return """   
+    Syntax : apr_spoofing.py [options]
+    
+    Options : 
+    --------
+    
+    -t, --time=     time before checking again arp table (default 3000 ms)
+    -v              Verbose mode (default none)
+    -a              Active analysis. Check in continue to avoid false positive (default none)
+    -V              Print the version of the software and exit
+    """
+
+def print_version():
+    return """
+    arp_spoofing. Python. GNU License. V 0.0.1. 2020-09
+    """
+
+if __name__ == "__main__" :
+    main()
---
+++ daemon_arp_spoofing.py
@@ -0,0 +1,63 @@
+import time
+from datetime import date, datetime
+import subprocess
+import syslog
+from scapy.all import sniff, ARPHDR_ETHER, ETHER_ANY, srp, conf
+from daemons.prefab import run
+
+
+class Daemon_arp_spoofing(run.RunDaemon):
+    """
+        active = detect all the request arp coming to change the arp table
+        verbose = that results are displayed on the screen
+        output = write a file with logs
+    """
+    def __init__(self, t=3000, active=False, output=False, verbose=False):
+        self.t = t
+        self.active = active
+        self.output = output
+        self.verbose = verbose
+
+        self.go = False
+
+    def restart(self):
+        """
+        Restart the daemon
+        :return: void
+        """
+        self.stop()
+        self.start()
+
+    def start(self):
+        self.go = True
+        self.run()
+
+    def stop(self):
+        self.go = False
+
+    def run(self):
+        self.go = True
+        if self.verbose:
+            print("Start analysis : "+datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+        arp_table_start = subprocess.getoutput("arp -a")
+
+        t1 = time.time()
+        d1 = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        while 1 and self.go:
+
+            apr_t = subprocess.getoutput("arp -a")
+
+            if apr_t != arp_table_start:
+                print("==> Arp poisoning attack detected at " + d1)
+                if self.verbose:
+                    syslog.syslog("Arp poisoning attack detected at " + d1)
+
+            if (time.time() - t1) * 1000 < self.t:
+                # sleep the time between the time execution and the time allowed by the user
+                time.sleep(self.t - ((time.time() - t1) * 1000))
+            # raffect t1 to calculate
+            t1 = time.time()
+            if self.output or self.verbose:
+                d1 = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
